33b3b9d7a4324f7659343a596d312f22f5a73bcf
DELL <DELL@DESKTOP-TD77G35>
2023-02-15 10:27:49 +0530

    send change at 10.25 am

---
+++ src/VelocityPOM_TESTclass/TC02_LogoutVCTCWebsite.java
@@ -0,0 +1,27 @@
+package VelocityPOM_TESTclass;
+
+public class TC02_LogoutVCTCWebsite 
+{
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hi i am Alam Hawaldar");
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
